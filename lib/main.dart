c5026df129667faa6d39c45e13f112fb7974cf85
Syed, Saad <saadsyed@my.unt.edu>
2025-10-16 03:14:06 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart'; // Import Firebase
 import 'login_screen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(NightGuardApp());
+  WidgetsFlutterBinding.ensureInitialized(); // Ensures Firebase can initialize
+  await Firebase.initializeApp(); // Initialize Firebase
+  runApp(MyApp());
 }
 
-class NightGuardApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'NightGuard',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: LoginScreen(),
