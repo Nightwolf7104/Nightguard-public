e09ff9828f3cd63c07ba2b78177a2a7c55dd1da8
Saad Syed <saadsyed@my.unt.edu>
2025-10-22 21:48:29 -0500

    Fix login redirect issue and update forms

--- nightguard_web/nightguard_web/urls.py
+++ nightguard_web/nightguard_web/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('escort.urls')),
+    path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="/login/")),  # root redirects to login
+    path("", include("escort.urls")),  # include escort app URLs
 ]
